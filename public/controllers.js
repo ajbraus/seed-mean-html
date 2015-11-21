c19b0b358e47c1f1dc1706ebb4ae6162938bd107
Adam Braus <adambraus@Six-Persimmons.local>
2015-11-20 16:28:39 -0800

    soltuion in controller

--- public/controllers.js
+++ public/controllers.js
@@ -10,12 +10,40 @@ angular.module('myApp.controllers', [])
   }])
 
   //POSTS
-  .controller('TodosIndexCtrl', ['$scope', '$location', function ($scope, $location) {
-    // GET TODOS
+  .controller('PostsIndexCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+    // GET POSTS
+    $http.get('/api/posts')
+      .success(function(data) {
+        $scope.posts = data;
+      })
+      .error(function(data) {
 
-    // NEW TODO
+      });
 
-    // CREATE A TODO
+    // NEW POST
+    $scope.post = {};
 
-    // DELETE A TODO
+    // CREATE A POST
+    $scope.createPost = function() {
+      $http.post('/api/posts', $scope.post)
+        .success(function(data){
+          $scope.posts.unshift(data);
+        })
+        .error(function(data) {
+          alert("there was a problem saving your post");
+        })
+      $scope.post = {};
+    }
+
+    // DELETE A POST
+    $scope.deletePost = function(post) {
+      $http.delete('/api/posts/' + post._id)
+        .success(function(data){
+          var index = $scope.posts.indexOf(post)
+          $scope.posts.splice(index, 1);          
+        })
+        .error(function(data) {
+
+        })
+    }
   }]);
